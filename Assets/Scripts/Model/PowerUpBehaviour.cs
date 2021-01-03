fac77a8b1aca045324ceaa90757da4f32874687b
GSerraclara <guillermo_sv96@hotmail.com>
2021-01-03 22:53:47 +0100

    base powerup

---
+++ Assets/Scripts/Model/PowerUpBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBehaviour : MonoBehaviour
+{
+    #region Attributes
+    #region Component References
+    public PowerUpController controller;
+
+    [SerializeField]
+    private PowerUp powerup;
+
+    private Renderer renderer_;
+
+    private Transform transform_;
+
+    public Material PowerupMaterial
+    {
+        get { return renderer_.material; }
+        set { renderer_.material = value; }
+    }
+    #endregion
+    
+
+    #endregion
+
+    #region Monobehaviour API
+
+    private void Awake()
+    {
+        transform_ = transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Importante si cambiamos el tag de player 
+        //para otros modos habra que modificar esto
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision");
+            ActivatePowerup();
+        }
+    }
+
+    #endregion
+
+    private void ActivatePowerup()
+    {
+        controller.ActivatePowerup(powerup);
+    }
+
+    public void SetPowerup(PowerUp powerup)
+    {
+        this.powerup = powerup;
+        gameObject.name = powerup.name;
+    }
+}
---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// como da problemas el camera follow he echo esto para tirar en test
+// a modo de apaño
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    private Vector3 cameraOffset;
+
+    [Range(0.01f,1.0f)]
+    public float smoothFactor = 0.5f;
+
+    public bool lookAtPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // it is called after Update method
+    void LateUpdate()
+    {
+        Vector3 newPos = PlayerTransform.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+        if (lookAtPlayer)
+        {
+            transform.LookAt(PlayerTransform);
+        }
+    }
+}
