59af7c80a93e588f436f9e339f355d4ebab64e1f
Oscar Pellice <oscar.pellice@students.salle.url.edu>
2021-02-19 00:10:28 +0100

    Quality of life

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,14 +32,25 @@ public class PlayerController : MonoBehaviour
     // Inputs
     private float horizontalInput;
     private float verticalInput;
+    private bool ableAutomatic = true;
 
     private PhotonView PV;
-    private Func<GameManager> gameManager;
+    private GameManager gameManager;
+
+    private List<PathReader.Moment> raceInfo;
+    private int nextNode = 0;
+    private Vector3 targetToGet;
+    private float tResta = 0;
+    private float tActual = 0;
+    private const float DistMin = 5f;
+    LineRenderer lineRenderer;
+    Transform car_transform;
 
     private void Awake()
     {
         PV = GetComponent<PhotonView>();
-        gameManager = FindObjectOfType<GameManager>;
+        gameManager = FindObjectOfType<GameManager>();
+        raceInfo = SaveInfo.Instance.ReturnJson();
     }
 
     // Start is called before the first frame update
@@ -48,11 +59,28 @@ public class PlayerController : MonoBehaviour
         if (!PV.IsMine)
         {
             Destroy(this.transform.Find("Camera").gameObject);
-            Destroy(this.transform.Find("Minimap").gameObject);
+            //Destroy(this.transform.Find("Minimap").gameObject);
         }
 
         // Resituem el centre de massa
         rb.centerOfMass = new Vector3 (0,-0.25f,0f); // Movem el centre de massa per que no giri
+
+        //For creating line renderer object
+        lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
+        lineRenderer.startColor = Color.black;
+        lineRenderer.endColor = Color.black;
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.positionCount = raceInfo.Count;
+        lineRenderer.useWorldSpace = true;
+
+        //For drawing line in the world space, provide the x,y,z values
+        for (int i = 0; i < raceInfo.Count; i++)
+        {
+            lineRenderer.SetPosition(i, new Vector3(raceInfo[i].position.x, 1, raceInfo[i].position.z)); //x,y and z position of the starting point of the line
+        }
+
+        car_transform = transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -60,6 +88,18 @@ public class PlayerController : MonoBehaviour
     {
         if (!PV.IsMine) return;
         GetInput();
+
+        if (ableAutomatic == true)
+        {
+            // Busquem al seguent node
+            tActual = Time.time - tResta;
+            if (tActual > raceInfo[nextNode].time && Vector3.Distance(rb.position, raceInfo[nextNode].position) < DistMin)
+            {
+                //tResta = tActual - raceInfo[nextNode].time;
+                targetToGet = raceInfo[nextNode].position;
+                nextNode = (nextNode + 1) % raceInfo.Count;
+            }
+        }
     }
 
     // Detectem inputs del teclat
@@ -67,6 +107,10 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.Q))
+        {
+            ableAutomatic = !ableAutomatic;
+        } 
         isBreaking = Input.GetKey(KeyCode.Space);
         isTabing = Input.GetKey(KeyCode.Tab);
     }
@@ -77,18 +121,55 @@ public class PlayerController : MonoBehaviour
         if (UIManager.Instance.tab && !isTabing) UIManager.Instance.tab = false;
         if (!UIManager.Instance.tab && isTabing) UIManager.Instance.tab = true;
 
-        HandleMotor();
-        HandleSteering();
-        UpdateWheels();
+        if (ableAutomatic == false)
+        {
+            HandleMotor(verticalInput);
+            HandleSteering(horizontalInput);
+            UpdateWheels();
+        } else
+        {
+            HandleVelocity();
+            HandleSteering(CalculateAngle());
+            UpdateWheels();
+        }
+        
+    }
+
+    // Calcula el angle entre el cotxe i el node
+    private int CalculateAngle()
+    {
+        Vector3 targetDir = new Vector3(targetToGet.x - car_transform.position.x,
+                                        targetToGet.y - car_transform.position.y,
+                                        targetToGet.z - car_transform.position.z);
+        Vector3 forward = car_transform.forward;
+        float angle = Vector3.SignedAngle(forward, targetDir, Vector3.up);
+        if (angle < -5.0f) return -1;
+        else if (angle > 5.0f) return 1;
+        return 0;
+    }
+
+    private void HandleVelocity()
+    {
+        //HandleMotor(0.5f);
+        // Com fer que es posi a la velocitat que hauria d'anar
+        if (raceInfo[nextNode].velocity > rb.velocity.magnitude)
+        {
+            HandleMotor((rb.velocity.magnitude + raceInfo[nextNode].velocity / 2) / 30);
+        }
+        else if (raceInfo[nextNode].velocity < rb.velocity.magnitude)
+        {
+            HandleMotor(0);
+        }
+        // Si diferencia entre actual i futura es molt inferior frenar
     }
 
     // Apliquem força de motor
-    private void HandleMotor()
+    private void HandleMotor(float multiply)
     {
-        rearLeftWheelCollider.motorTorque = motorForce * verticalInput;
-        rearRightWheelCollider.motorTorque = motorForce * verticalInput; 
-        frontLeftWheelCollider.motorTorque = motorForce * verticalInput;
-        frontRightWheelCollider.motorTorque = motorForce * verticalInput;
+        rearLeftWheelCollider.motorTorque = motorForce * multiply;
+        rearRightWheelCollider.motorTorque = motorForce * multiply; 
+        frontLeftWheelCollider.motorTorque = motorForce * multiply;
+        frontRightWheelCollider.motorTorque = motorForce * multiply;
 
         currentbreakForce = isBreaking ? breakForce : 0f;
         rearLeftWheelCollider.brakeTorque = currentbreakForce;
@@ -96,11 +177,10 @@ public class PlayerController : MonoBehaviour
     }
 
     // Rota les rodes
-    private void HandleSteering()
+    private void HandleSteering(float multiply)
     {
-        currentSteerAngle = maxSteerAngle * horizontalInput;
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle;
+        frontLeftWheelCollider.steerAngle = maxSteerAngle * multiply;
+        frontRightWheelCollider.steerAngle = maxSteerAngle * multiply;
     }
 
     // Situa els meshes on els transforms
