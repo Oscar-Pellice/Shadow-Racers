8a568f2fa91c909944e46b67c657b75584efb617
David Sánchez Martínez <david.sanchez@students.salle.url.edu>
2021-05-07 17:58:06 +0200

    MenuFiniquitao

--- Assets/Scripts/PhotonScripts/SettingsMenu.cs
+++ Assets/Scripts/PhotonScripts/SettingsMenu.cs
@@ -2,11 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public AudioMixer soundMixer;
+    public Dropdown resolutionDropdown; 
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0; 
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i; 
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue(); 
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     public void setSound (float sound)
     {
@@ -19,5 +53,4 @@ public class SettingsMenu : MonoBehaviour
         InfoSaver.Instance.volume = volume;
         audioMixer.SetFloat ("volume", volume);
     }
-
 }
